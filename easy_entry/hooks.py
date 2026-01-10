100024624dceef21aebcfa4f8e2e953b4b39a559
Abdelrahman <bedoelsayed785@gmail.com>
2026-01-10 11:27:46 +0200

    make it list

--- easy_entry/hooks.py
+++ easy_entry/hooks.py
@@ -28,7 +28,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/easy_entry/css/easy_entry.css"
-app_include_js = "public/js/pos_guard.js"
+app_include_js = ["public/js/pos_guard.js"]
 # include js, css files in header of web template
 # web_include_css = "/assets/easy_entry/css/easy_entry.css"
 # web_include_js = "/assets/easy_entry/js/easy_entry.js"
